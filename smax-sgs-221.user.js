6b679d10283aaff4b6c785b6bb2ea7c5d215259b
oadrianocardoso <oadrianocardoso@gmail.com>
2025-11-28 16:05:08 -0300

    Update smax-sgs-221.user.js

--- smax-sgs-221.user.js
+++ smax-sgs-221.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SMAX SGS 221
 // @namespace    https://github.com/oadrianocardoso/smax-sgs-221
-// @version      3.12
+// @version      5
 // @description  SMAX SGS 221
 // @author       ADRIANO AUGUSTO CARDOSO E SANTOS
 // @match        https://suporte.tjsp.jus.br/saw/*
