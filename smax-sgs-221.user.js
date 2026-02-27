f1fd7ce58e52f4a0376f5f2450a848e2e4ecc6be
oadrianocardoso <oadrianocardoso@gmail.com>
2026-02-27 14:19:50 -0300

    Correção no visualizador de anexos.

--- smax-sgs-221.user.js
+++ smax-sgs-221.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SMAX SGS 221
 // @namespace    https://github.com/oadrianocardoso/smax-sgs-221
-// @version      10.0
+// @version      10.1
 // @description  SMAX SGS 221 (modularizado)
 // @author       ADRIANO AUGUSTO CARDOSO E SANTOS
 // @match        https://suporte.tjsp.jus.br/saw/*
