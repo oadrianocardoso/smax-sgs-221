8e215679b3d2c666c0d5fac8757a1ffe747c1dda
Adriano <oadrianocardoso@gmail.com>
2026-09-01 15:54:07 -0300

    v21

--- smax-sgs-221.user.js
+++ smax-sgs-221.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SMAX SGS 221
 // @namespace    https://github.com/oadrianocardoso/smax-sgs-221
-// @version      20
+// @version      21
 // @description  SMAX SGS 221 (modularizado)
 // @author       ADRIANO AUGUSTO CARDOSO E SANTOS
 // @match        https://suporte.tjsp.jus.br/saw/*
@@ -29,10 +29,10 @@
 // @require      https://raw.githubusercontent.com/oadrianocardoso/smax-sgs-221/main/modules/comments.js?v=15
 // @require      https://raw.githubusercontent.com/oadrianocardoso/smax-sgs-221/main/modules/discussion-api.js?v=15
 // @require      https://raw.githubusercontent.com/oadrianocardoso/smax-sgs-221/main/modules/followers-alert.js?v=17
-// @require      https://raw.githubusercontent.com/oadrianocardoso/smax-sgs-221/main/modules/discussion-advisor.js?v=20
+// @require      https://raw.githubusercontent.com/oadrianocardoso/smax-sgs-221/main/modules/discussion-advisor.js?v=21
 // @require      https://raw.githubusercontent.com/oadrianocardoso/smax-sgs-221/main/modules/sections.js?v=18
 // @require      https://raw.githubusercontent.com/oadrianocardoso/smax-sgs-221/main/modules/zen-mode.js?v=17
-// @require      https://raw.githubusercontent.com/oadrianocardoso/smax-sgs-221/main/modules/attachments.js?v=20
+// @require      https://raw.githubusercontent.com/oadrianocardoso/smax-sgs-221/main/modules/attachments.js?v=21
 // @require      https://raw.githubusercontent.com/oadrianocardoso/smax-sgs-221/main/modules/detratores.js?v=15
 // @require      https://raw.githubusercontent.com/oadrianocardoso/smax-sgs-221/main/modules/botoes-resolucao.js?v=18
 // @require      https://raw.githubusercontent.com/oadrianocardoso/smax-sgs-221/main/modules/destaca-atendente.js?v=15
